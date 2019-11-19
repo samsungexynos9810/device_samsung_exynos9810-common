89a367738cb0bd498138931d02eff71a9a0b87bc
SeYeong Byeon <sy.byeon@samsung.com>
2019-11-19 14:02:07 +0900

    exynos9810-common: gralloc3: [g19] make ion handle import as a feature
    Do not import ion handle for debugging in 7885 based SOCs.
    
    Change-Id: I5b47d1519499ce70eeb58f2335ad0deb2ae342e8
    Signed-off-by: SeYeong Byeon <sy.byeon@samsung.com>

--- hardware/gralloc3/src/mali_gralloc_reference.cpp
+++ hardware/gralloc3/src/mali_gralloc_reference.cpp
@@ -66,12 +66,14 @@ int mali_gralloc_reference_retain(mali_gralloc_module const *module, buffer_hand
 	else if (hnd->flags & (private_handle_t::PRIV_FLAGS_USES_ION))
 	{
 		retval = mali_gralloc_ion_map(hnd);
+#if GRALLOC_ION_HANDLE_IMPORT == 1
 		if (retval >= 0)
 		{
 			/* failing to import ion handle is not fatal */
 			import_exynos_ion_handles(hnd);
 			retval = 0;
 		}
+#endif
 	}
 	else
 	{
@@ -117,7 +119,9 @@ int mali_gralloc_reference_release(mali_gralloc_module const *module, buffer_han
 				mali_gralloc_dump_buffer_erase(hnd);
 			}
 
+#if GRALLOC_ION_HANDLE_IMPORT == 1
 			free_exynos_ion_handles(hnd);
+#endif
 			mali_gralloc_buffer_free(handle);
 			delete handle;
 		}
@@ -132,7 +136,9 @@ int mali_gralloc_reference_release(mali_gralloc_module const *module, buffer_han
 			if (hnd->flags & (private_handle_t::PRIV_FLAGS_USES_ION))
 			{
 				mali_gralloc_ion_unmap(hnd);
+#if GRALLOC_ION_HANDLE_IMPORT == 1
 				free_exynos_ion_handles(hnd);
+#endif
 			}
 			else
 			{
