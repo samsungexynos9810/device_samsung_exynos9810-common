5447941e1fea5066385cfe7a991267136d742092
Josh Wu <joshwu@google.com>
2022-08-15 21:50:57 +0100

    exynos9810-common: bluetooth: migrate to AIDL audio HAL

--- common.mk
+++ common.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.1-impl:32 \
+    android.hardware.bluetooth.audio-impl:32 \
     android.hidl.allocator@1.0.vendor:32 \
     audio.bluetooth.default \
     audio.r_submix.default \
