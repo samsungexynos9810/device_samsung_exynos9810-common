5170087420987ad0f7325d54091f55a8447c94e4
Chris Crump <chriscrawford893@gmail.com>
2022-11-05 18:06:09 +0000

    exynos9810-common: add AOSP Bluetooth soong namespace

--- common-aospa.mk
+++ common-aospa.mk
@@ -13,3 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Soong Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    packages/modules/Bluetooth/android/app
